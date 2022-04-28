6498bf171e753f62a88babf10d7f09176ee0f6e8
Jan Bleimling <jan.bleimling@uni-bielefeld.de>
2022-04-28 17:18:49 +0200

    Transferred files from other repo

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash               
+
+# This script configures git hooks to be symlinked to ./hooks and creates a new conda environment based on the .yml file
+
+envname="dona-env"
+
+install_hooks() {
+  git config core.hooksPath \
+    || git config core.hooksPath ./hooks
+  echo "Hooks configured sucessfully"
+}
+
+create_env() {
+  read -p "Continue to create environment $envname? (y/n) " answer
+  if [ "$answer" == "y" ]
+  then
+    conda env create --name $envname --file $envname.yml
+  else
+    echo "Aborting"
+  fi
+}
+
+install_hooks
+create_env
